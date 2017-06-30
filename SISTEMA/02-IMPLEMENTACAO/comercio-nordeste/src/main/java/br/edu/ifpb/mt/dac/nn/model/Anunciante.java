95e48a9f978c99bd40080112f054c158575c35bb
Andre Luis <tr.andreluis@gmail.com>
2017-06-30 09:34:51 -0300

    Criação das entitades modelo e mapeamento das mesmas com JPA - Hibernate

---
+++ SISTEMA/02-IMPLEMENTACAO/comercio-nordeste/src/main/java/br/edu/ifpb/mt/dac/nn/model/Anunciante.java
@@ -0,0 +1,113 @@
+package br.edu.ifpb.mt.dac.nn.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import br.edu.ifpb.mt.dac.nn.enumerations.NivelAnunciante;
+
+@Entity
+public class Anunciante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "NOME")
+	private String nome;
+
+	@Column(name = "NOME_USUARIO")
+	private String nomeUsuario;
+
+	@Column(name = "DATA_NASCIMENTO")
+	private LocalDate dataNascimento;
+	
+	@Column(name = "EMAIL")
+	private String email;
+	
+	@Column(name = "SENHA")
+	private String senha;
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = { CascadeType.ALL }, mappedBy = "anunciante")
+	private List<Anuncio> anuncios;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "NIVEL")
+	private NivelAnunciante nivel;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public List<Anuncio> getAnuncios() {
+		return anuncios;
+	}
+
+	public void setAnuncios(List<Anuncio> anuncios) {
+		this.anuncios = anuncios;
+	}
+
+	public NivelAnunciante getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(NivelAnunciante nivel) {
+		this.nivel = nivel;
+	}
+
+}
---
+++ SISTEMA/02-IMPLEMENTACAO/comercio-nordeste/src/main/java/br/edu/ifpb/mt/dac/nn/model/Anuncio.java
@@ -0,0 +1,86 @@
+package br.edu.ifpb.mt.dac.nn.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Anuncio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "TITULO")
+	private String titulo;
+	
+	@Column(name = "DESCRICAO")
+	private String descricao;
+	
+	@Column(name = "PRECO")
+	private Double preco;
+
+	@Column(name = "DATA_PUCLICACAO")
+	private LocalDateTime dataHoraPublicacao;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "ANUNCIANTE_FK")
+	private Anunciante anunciante;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public LocalDateTime getDataHoraPublicacao() {
+		return dataHoraPublicacao;
+	}
+
+	public void setDataHoraPublicacao(LocalDateTime dataHoraPublicacao) {
+		this.dataHoraPublicacao = dataHoraPublicacao;
+	}
+
+	public Anunciante getAnunciante() {
+		return anunciante;
+	}
+
+	public void setAnunciante(Anunciante anunciante) {
+		this.anunciante = anunciante;
+	}
+	
+}
