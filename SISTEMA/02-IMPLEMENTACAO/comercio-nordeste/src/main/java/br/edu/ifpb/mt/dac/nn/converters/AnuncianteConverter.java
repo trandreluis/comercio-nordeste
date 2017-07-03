3d16d90421455a21706a6fac99f42972383b56fe
Andre Luis <tr.andreluis@gmail.com>
2017-07-03 17:28:27 -0300

    Implementacao do converter para Anunciantes

---
+++ SISTEMA/02-IMPLEMENTACAO/comercio-nordeste/src/main/java/br/edu/ifpb/mt/dac/nn/converters/AnuncianteConverter.java
@@ -0,0 +1,51 @@
+package br.edu.ifpb.mt.dac.nn.converters;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+import br.edu.ifpb.mt.dac.nn.model.Anunciante;
+import br.edu.ifpb.mt.dac.nn.service.AnuncianteService;
+import br.edu.ifpb.mt.dac.nn.service.impl.AnuncianteServiceImpl;
+
+@RequestScoped
+@FacesConverter(forClass=Anunciante.class)
+public class AnuncianteConverter implements Converter {
+
+	private AnuncianteService anuncianteService;
+	
+	public AnuncianteConverter() {
+		this.anuncianteService = new AnuncianteServiceImpl();
+	}
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		if (value == null || value.trim().isEmpty()) {
+			return null;
+		}
+		Long id = 0l;
+		try {
+			id = Long.parseLong(value);
+			return anuncianteService.buscarPorID(id);
+		} catch (NumberFormatException e) {
+			throw new ConverterException(new FacesMessage(String.format(
+					"%s é invalido para o anunciante", id)), e);
+		}
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		if (value == null) {
+			return null;
+		}
+		Long id = ((Anunciante) value).getId();
+		return (id != null) ? id.toString() : null;
+	}
+
+}
