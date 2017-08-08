6d9cd9ad0b1ff3bd680f329ebea53d7f34de1651
Andre Luis <tr.andreluis@gmail.com>
2017-08-08 05:15:23 -0300

    Google Matrix Distance API: otimização dos resultados com base na distância

--- SISTEMA/02-IMPLEMENTACAO/negocio-nordeste/src/main/java/br/edu/ifpb/mt/dac/nn/beans/ResultadoBuscaBean.java
+++ SISTEMA/02-IMPLEMENTACAO/negocio-nordeste/src/main/java/br/edu/ifpb/mt/dac/nn/beans/ResultadoBuscaBean.java
@@ -19,6 +19,7 @@ import br.edu.ifpb.mt.dac.nn.services.AnuncioService;
 import br.edu.ifpb.mt.dac.nn.services.ContaService;
 import br.edu.ifpb.mt.dac.nn.services.impl.DistanciaServiceImpl;
 import br.edu.ifpb.mt.dac.nn.util.jsf.JSFUtils;
+import br.edu.ifpb.mt.dac.nn.util.json.LocalizacaoUtils;
 import br.edu.ifpb.mt.dac.nn.util.mensagens.MessageUtils;
 
 @Named
@@ -30,6 +31,7 @@ public class ResultadoBuscaBean extends AbstractBean implements Serializable {
 	private String textoBotaoNovo;
 	private String paginaBotaoNovo = "";
 	private String stringDeBusca;
+	private Localizacao localizacao;
 	@Inject
 	private ContaService contaService;
 	@Inject
@@ -43,18 +45,39 @@ public class ResultadoBuscaBean extends AbstractBean implements Serializable {
 	private Cidade cidadeSelecionada;
 	private Estado estadoSelecionado;
 	private List<Anuncio> resultadoBusca;
+	private boolean buscaOtimizada;
 
 	public void preRenderView() {
 		conta = contaService.buscarPorUsername(getUsernameUsuarioLogado());
 		MessageUtils.messageSucess("String de busca: " + stringDeBusca);
 		
+		resultadoBusca = null;
+
 		try {
-			resultadoBusca = anuncioService.buscaPorTag(stringDeBusca);
+			if (buscaOtimizada) {
+				
+
+				LocalizacaoUtils util = new LocalizacaoUtils();
+				Localizacao localizacao = new Localizacao();
+				for (Cidade cidade : cidadesDoEstado) {
+					if (cidade.getId().equals(idCidadeSelecionada)) {
+						Estado estado = util.buscarEstadoPorCodigo(idEstadoSelecionado);
+						localizacao.setCidade(cidade.getNome());
+						localizacao.setEstado(estado.getNome());
+					}
+				}
+				
+				DistanciaServiceImpl distancia = new DistanciaServiceImpl();
+				resultadoBusca = anuncioService.buscaPorTag(stringDeBusca);
+				resultadoBusca = distancia.buscaOtimizadaPelaDistancia(localizacao, resultadoBusca);
+			} else {
+				resultadoBusca = anuncioService.buscaPorTag(stringDeBusca);
+			}
 		} catch (NegocioNordesteException e) {
-			MessageUtils.messageError("Houve um erro ao relizar a busca no banco.");
+			MessageUtils.messageError(e.getMessage());
 			e.printStackTrace();
 		}
-		
+
 		if (conta == null) {
 			textoBotaoNovo = "Novo";
 			paginaBotaoNovo = "/cadastro_anunciante.xhtml?faces-redirect=true";
@@ -63,25 +86,33 @@ public class ResultadoBuscaBean extends AbstractBean implements Serializable {
 			textoBotaoNovo = "Minha conta";
 			paginaBotaoNovo = "/paginas/anunciante/anunciante.xhtml?faces-redirect=true";
 		}
-		
-		//TESTE
-		DistanciaServiceImpl distancia = new DistanciaServiceImpl();
-		Localizacao localizacao = new Localizacao();
-		localizacao.setCidade("Sertânia");
-		localizacao.setEstado("Pernambuco");
-		
-		List<Anuncio> anuncios = anuncioService.buscarTodos();
-		
-		try {
-			distancia.buscaOtimizadaPelaDistancia(localizacao, anuncios);
-		} catch (NegocioNordesteException e) {
-			MessageUtils.messageError("Deu merda na otimização");
-		}
-		
-		//FIM TESTE
-		
+
+	}
+	
+	public boolean isBuscaOtimizada() {
+		return buscaOtimizada;
 	}
 
+	public void setBuscaOtimizada(boolean buscaOtimizada) {
+		this.buscaOtimizada = buscaOtimizada;
+	}
+
+	public void carregarCidadesDoEstado() {
+		LocalizacaoUtils localizacao = new LocalizacaoUtils();
+		estadoSelecionado = localizacao.buscarEstadoPorCodigo(idEstadoSelecionado);
+		cidadesDoEstado = localizacao.buscarCidadesPorCodigoDoEstado(estadoSelecionado.getId());
+	}
+
+	public void carregarEstadosECidades() {
+		LocalizacaoUtils localizacao = new LocalizacaoUtils();
+		estadosNordeste = localizacao.buscarEstadosDoNordeste();
+		if (idEstadoSelecionado == null) {
+			cidadesDoEstado = localizacao.buscarCidadesPorCodigoDoEstado("2");
+		} else {
+			cidadesDoEstado = localizacao.buscarCidadesPorCodigoDoEstado(idEstadoSelecionado);
+		}
+	}
+	
 	public void visualizarAnuncio(Anuncio anuncio) {
 		if (anuncio != null) {
 			JSFUtils.setParam("anuncio", anuncio);
@@ -183,6 +214,14 @@ public class ResultadoBuscaBean extends AbstractBean implements Serializable {
 
 	public void setStringDeBusca(String stringDeBusca) {
 		this.stringDeBusca = stringDeBusca;
+	}
+
+	public Localizacao getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(Localizacao localizacao) {
+		this.localizacao = localizacao;
 	}
 
 }
